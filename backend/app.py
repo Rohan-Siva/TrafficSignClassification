0e988109d1cdd1671c22a87866b57338e054efa4
Rohan Siva <rohansiva123@gmail.com>
2025-01-28 21:22:56 -0600

    test flask+react

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
